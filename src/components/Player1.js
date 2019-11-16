a324cab62f8aa2c1393869db4122fdeeb07f0f24
Joonas Suonperä <joonas.suonpera@gmail.com>
2019-11-16 13:26:28 +0200

    added dynamic player image src, based on player sstate. changed image folder structure and filenames

--- src/components/Player1.js
+++ src/components/Player1.js
@@ -2,6 +2,22 @@ import React, { Component } from "react";
 
 export default class Player1 extends Component {
     render() {
+		let health;
+        let playerHealth;
+
+        playerHealth = this.props.data.health;
+        if (playerHealth >= 8) {
+            health = 4;
+        } else if (playerHealth >= 6) {
+            health = 3;
+        } else if (playerHealth >= 4) {
+            health = 2;
+        } else if (playerHealth >= 2) {
+            health = 1;
+        } else {
+            health = 0;
+		}
+		
         let amount = Math.floor(this.props.data.health / 2);
         let half = this.props.data.health % 2;
         return (
@@ -32,7 +48,7 @@ export default class Player1 extends Component {
                 </div>
                 <div className="player1-image">
                     <img
-                        src="/images/Player_1_naked/Player 1 naked.png"
+                        src={'/images/Player1/Player1_' + this.props.data.armor + '_' + this.props.data.weapon + '_' + health + '.png'}
                         alt="Char"
                     ></img>
                 </div>
--- src/components/Player2.js
+++ src/components/Player2.js
@@ -2,10 +2,31 @@ import React, { Component } from "react";
 
 export default class Player2 extends Component {
     render() {
+        let health;
+        let playerHealth;
+
+        playerHealth = this.props.data.health;
+        if (playerHealth >= 8) {
+            health = 4;
+        } else if (playerHealth >= 6) {
+            health = 3;
+        } else if (playerHealth >= 4) {
+            health = 2;
+        } else if (playerHealth >= 2) {
+            health = 1;
+        } else {
+            health = 0;
+        }
+        
         return (
             <div className="player2-div">
                 <div className="player2-actions"></div>
-                <div className="player2-image"></div>
+                <div className="player2-image">
+                    <img 
+                        src={'/images/Player2/Player2_' + this.props.data.armor + '_' + this.props.data.weapon + '_' + health + '.png'}
+                        alt=""
+                    />
+                </div>
                 <div className="player2-status">
 					<div className="player2-gold">
 						<h2>Gold</h2>
