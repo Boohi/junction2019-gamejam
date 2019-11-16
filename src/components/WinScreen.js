1454882e70f4c9c2c7d026ff8decd76cdfdd05c4
Lensku <henri.lencioni@aalto.fi>
2019-11-16 21:45:21 +0200

    Winscreen animation v.0.0 done

--- src/components/WinScreen.js
+++ src/components/WinScreen.js
@@ -2,13 +2,27 @@ import React, { Component } from "react";
 import "./WinScreen.scss";
 
 export default class WinScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { p1med: "p1winDown.png", p2med: "p2loseLeft.png" };
+    }
+    componentDidMount() {
+        setInterval(this.changePictures.bind(this), 500);
+    }
+    changePictures() {
+        if (this.state.p1med === "p1winDown.png") {
+            this.setState({ p1med: "p1winUp.png", p2med: "p2loseRight.png" });
+        } else {
+            this.setState({ p1med: "p1winDown.png", p2med: "p2loseLeft.png" });
+        }
+    }
     render() {
         console.log("test");
         return (
             <div className="add-grid">
                 <div className="add-player1">
                     <img
-                        src="p1winDown.png"
+                        src={"/" + this.state.p1med}
                         alt="player 1"
                         width="250"
                         height="500"
@@ -20,7 +34,7 @@ export default class WinScreen extends Component {
                 </div>
                 <div className="add-player2">
                     <img
-                        src="p2loseLeft.png"
+                        src={"/" + this.state.p2med}
                         alt="player 2"
                         width="250"
                         height="500"
