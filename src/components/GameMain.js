917009ddbb19b6c797644fb821111ce3bb481566
Otto Mäenpää <otto.maenpaa@gmail.com>
2019-11-16 02:25:06 +0200

    cleaned up the code a little

--- src/components/GameMain.js
+++ src/components/GameMain.js
@@ -1,14 +1,5 @@
 import React, { Component } from "react";
 import "./GameMain.scss";
-let clientId =
-    "133675380584-86ac3ndr5m7il6b79l5lqr5jitc8g6a1.apps.googleusercontent.com";
-let apiKey = "AIzaSyC86syVDiTJq6RTioJoXeEb17biIE0meXc";
-const scopes =
-    "https://www.googleapis.com/auth/gmail.readonly " +
-    "https://www.googleapis.com/auth/gmail.send";
-let discoveryDocs = [
-    "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"
-];
 
 export default class GameMain extends Component {
     constructor(props) {
@@ -19,7 +10,6 @@ export default class GameMain extends Component {
             player2Mail: "joonas.suonpera@gmail.com",
             player1Message: {},
             player2Message: {},
-            newestMessage: {}
         };
 
         this.getPageOfMessages = this.getPageOfMessages.bind(this);
@@ -54,7 +44,6 @@ export default class GameMain extends Component {
     }
 
     getPageOfMessages(start, limit = 5) {
-        /* return new Promise(function(resolve, reject) { */
         let messages = [];
 
         let getMessagesRequest = window.gapi.client.gmail.users.messages.list({
@@ -65,14 +54,7 @@ export default class GameMain extends Component {
 
         getMessagesRequest.execute(response => {
             let numberOfMessageDetailsToFetch = response.messages.length;
-            let messageIdToArrayIndexMap = {};
-
             response.messages.forEach((message, messageIndex) => {
-                //
-                // We put each message "stub" here. This means that "messages" will contain newest to oldest messages
-                // in the same order as you'd see them at gmail.com. Later, we'll retrieve the details for each message.
-                // When we do that, those details need to be put into the correct position/slot in "messages."
-                //
                 messages.push(message);
 
                 let messageDetailsRequest = window.gapi.client.gmail.users.messages.get(
@@ -112,28 +94,14 @@ export default class GameMain extends Component {
                         snippet: snippet,
                         date: date
                     });
-                    //console.log(messageDetails.payload.headers[15].value);
-                    //console.log(messageDetails.id, messageDetails.snippet, messageDetails.payload.headers[22].value, messageDetails.payload.headers[15].value);
-
-                    //
-                    // Replace the stub with the retrieved message. This allows us to get message details at different times
-                    // (some take longer than others), but the message-with-details are in the same order as the original
-                    // list we retrieved.
-                    //
-                    //messages[messageIndex] = parseMessageDetails(messageDetails)
 
                     numberOfMessageDetailsToFetch -= 1;
                     if (numberOfMessageDetailsToFetch === 0) {
                         return;
-                        /* resolve(messages); */
                     }
                 });
-                /* console.log(message); */
-                /* this.getMessageBody(message); */
-                /* console.log(messages); */
             });
         });
-        /* }); */ // promise
     }
 
 
@@ -154,125 +122,4 @@ export default class GameMain extends Component {
             </div>
         );
     }
-    /* 	componentDidMount() {
-		this.authClient();
-		window.gapi.load('client:auth2', '');
-	} */
-
-    /* 	function initClient() {
-		window.gapi.client.init({
-			apiKey: apiKey,
-			discoveryDocs: discoveryDocs,
-			clientId: clientId,
-			scope: scopes
-		}).then(function () {
-		  // Listen for sign-in state changes.
-		  window.gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-		  // Handle the initial sign-in state.
-		  updateSigninStatus(window.gapi.auth2.getAuthInstance().isSignedIn.get());
-		  authorizeButton.onclick = handleAuthClick;
-		  signoutButton.onclick = handleSignoutClick;
-		});
-	  } */
-
-    /* 	async authClient() {
-		window.gapi.client.init({
-			apiKey: key,
-			discoveryDocs: discoveryDocs,
-			clientId: clientId,
-			scope: scopes
-		})
-		let auth2 = await loadAuth2(clientId, scopes);
-	}
-	
-	render() {
-		return (
-			<div>
-				<h1>Hive goes to Junction 2019</h1>
-				<h3>GAME JAM</h3>
-				<div id="authorize-button"></div>
-				<div id="signout-button"></div>
-			</div>
-		)
-	} */
-    /* 	constructor(props) {
-        super(props);
-        this.state = {
-            user: null
-        }
-    }
-    async componentDidMount() {
-		let auth2 = await loadAuth2(clientId, scopes);
-
-        if (auth2.isSignedIn.get()) {
-            this.updateUser(auth2.currentUser.get())
-        } else {
-            this.attachSignin(document.getElementById('customBtn'), auth2);
-        }
-    }
-    async componentDidUpdate() {
-        if(!this.state.user) {
-			let auth2 = await loadAuth2(clientId, '')
-			window.gapi.client.load('gmail', 'v1', _ => {
-				return true;
-			 });
-            this.attachSignin(document.getElementById('customBtn'), auth2);
-		}
-		let initialRequest;
-		console.log(window.gapi.client);
-		setInterval(() => {
-			if (window.gapi.client) {
-				initialRequest = window.gapi.client.gmail.users.messages.list({
-					'userId': 'me'
-				  });
-				console.log(initialRequest);
-			} else {
-				console.log(window.gapi);
-			}
-		}, 2000)
-    }
-    updateUser(currentUser) {
-        let name = currentUser.getBasicProfile().getName()
-        let profileImg = currentUser.getBasicProfile().getImageUrl()
-        this.setState({
-            user: {
-                name: name,
-                profileImg: profileImg
-            }
-        })
-    }
-    attachSignin(element, auth2) {
-        auth2.attachClickHandler(element, {},
-            (googleUser) => {
-                this.updateUser(googleUser);
-            }, (error) => {
-                console.log(JSON.stringify(error))
-            });
-    }
-    signOut = () => {
-        let auth2 = window.gapi.auth2.getAuthInstance();
-        auth2.signOut().then(() => {
-            this.setState({ user: null })
-            console.log('User signed out.');
-        });
-    }
-    render() {
-        if(this.state.user) {
-            return (
-                <div className="container">
-                    <div id="" className="btn logout" onClick={this.signOut}>
-                        Logout
-                    </div>
-                </div>
-            );
-        } else {
-            return (
-                <div className="container">
-                    <div id="customBtn" className="btn login">
-                        Login
-                    </div>
-                </div>
-            );
-        }
-    } */
 }
--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,13 +1,4 @@
 import React, { Component } from "react";
-let clientId =
-    "133675380584-86ac3ndr5m7il6b79l5lqr5jitc8g6a1.apps.googleusercontent.com";
-let apiKey = "AIzaSyC86syVDiTJq6RTioJoXeEb17biIE0meXc";
-const scopes =
-    "https://www.googleapis.com/auth/gmail.readonly " +
-    "https://www.googleapis.com/auth/gmail.send";
-let discoveryDocs = [
-    "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest"
-];
 
 export default class StartScreen extends Component {
     render() {
